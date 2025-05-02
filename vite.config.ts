f4cd83e8e4694415a911ac83738bcdad85ea2005
hyunjin <71summernight@naver.com>
2025-05-03 01:43:05 +0900

    배포준비

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/front_5th_chapter2-3/",
   server: {
     proxy: {
       "/api": {

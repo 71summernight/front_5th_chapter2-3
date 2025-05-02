e27cd97445c2caa44169ae30633e77258f25698c
hyunjin <71summernight@naver.com>
2025-05-03 01:52:21 +0900

    api 주소 변경

--- src/shared/utils/apiUtils.ts
+++ src/shared/utils/apiUtils.ts
@@ -5,59 +5,59 @@ import { apiClient } from "./apiClient"
 export const commonApi = {
   // USER API
   async fetchUsers() {
-    const res = await apiClient.get<{ users: BasicUser[] }>("/api/users?limit=0&select=username,image")
+    const res = await apiClient.get<{ users: BasicUser[] }>("/users?limit=0&select=username,image")
     return res.users
   },
 
   // TAG API
   fetchTags: async () => {
-    const res = await apiClient.get("/api/posts/tags")
+    const res = await apiClient.get("/posts/tags")
     return res
   },
 
   // POST API
   async fetchPosts(limit: number, skip: number) {
-    return await apiClient.get<PostResponse>(`/api/posts?limit=${limit}&skip=${skip}`)
+    return await apiClient.get<PostResponse>(`/posts?limit=${limit}&skip=${skip}`)
   },
 
   async fetchPostsByTag(tag: string, limit: number, skip: number) {
-    return await apiClient.get<PostResponse>(`/api/posts/tag/${tag}?limit=${limit}&skip=${skip}`)
+    return await apiClient.get<PostResponse>(`/posts/tag/${tag}?limit=${limit}&skip=${skip}`)
   },
 
   async addPost(post: Omit<Post, "id">) {
-    return await apiClient.post<Post>("/api/posts/add", post)
+    return await apiClient.post<Post>("/posts/add", post)
   },
 
   async updatePost(post: Post) {
-    return await apiClient.put<Post>(`/api/posts/${post.id}`, post)
+    return await apiClient.put<Post>(`/posts/${post.id}`, post)
   },
 
   async deletePost(id: number) {
-    return await apiClient.delete(`/api/posts/${id}`)
+    return await apiClient.delete(`/posts/${id}`)
   },
 
   async searchPosts(query: string) {
-    return await apiClient.get<PostResponse>(`/api/posts/search?query=${query}`)
+    return await apiClient.get<PostResponse>(`/posts/search?query=${query}`)
   },
 
   // COMMENT API
   async fetchComments(postId: number) {
-    return await apiClient.get<FetchCommentsResponse>(`/api/comments/post/${postId}`)
+    return await apiClient.get<FetchCommentsResponse>(`/comments/post/${postId}`)
   },
 
   async addComment(comment: NewCommentInput) {
-    return await apiClient.post<Comment>("/api/comments/add", comment)
+    return await apiClient.post<Comment>("/comments/add", comment)
   },
 
   async updateComment(comment: Pick<Comment, "id" | "body">) {
-    return await apiClient.put<Comment>(`/api/comments/${comment.id}`, { body: comment.body })
+    return await apiClient.put<Comment>(`/comments/${comment.id}`, { body: comment.body })
   },
 
   async deleteComment(id: number) {
-    return await apiClient.delete(`/api/comments/${id}`)
+    return await apiClient.delete(`/comments/${id}`)
   },
 
   async likeComment(id: number, likes: number) {
-    return await apiClient.patch<Comment>(`/api/comments/${id}`, { likes })
+    return await apiClient.patch<Comment>(`/comments/${id}`, { likes })
   },
 }
